9ed1b8a196d7c913648a74c9fe0f6bdb17363dd9
Abdullah Usman <179724875+AbdullahUsman07@users.noreply.github.com>
2026-09-21 11:29:05 +0500

    feat: completed upto part 5

--- week_2/course_roster_lab.dart
+++ week_2/course_roster_lab.dart
@@ -1,7 +1,7 @@
 void main(List<String> args) {
   printWelcome("Course Roster Manager");
 
-  // part-2
+  // --------------- part-2
   const int maxCapacity = 4;
   final DateTime createdAt = DateTime.now();
   String courseTitle = 'CS442: Mobile Application Developement';
@@ -17,7 +17,7 @@ void main(List<String> args) {
     "${courseTitle} | Capacity: ${capacity} | Enrolled: ${enrolledStudents.length}",
   );
 
-  // part-3
+  // ------------ part-3 ---------------
 
   // part-3 (i, ii, iv)
   String? instructorEmail;
@@ -31,7 +31,7 @@ void main(List<String> args) {
   enrollmentCode = generateCode(courseTitle);
   print("Enrollment Code: ${enrollmentCode}");
 
-  // Part-4 (Formatting Strings)
+  // ------------------Part-4 (Formatting Strings) ----------------------
   String rawNames = ' Saqlain, Hanan ,Ahmed , Jamil ';
   List<String> cleanNames = [];
   for (var name in rawNames.split(',')) {
@@ -47,6 +47,39 @@ Credits: $creditHours
 ''';
   print(courseDescription);
   print('Seats left: ${capacity - enrolledStudents.length}');
+
+
+  // ------- Part 5 (Operators in Action) ----------
+  // Part-5 (i)
+  int fullGroups = enrolledStudents.length ~/ 3;
+  int leftover = enrolledStudents.length % 3;
+  print('Full Group of 3: ${fullGroups} , Leftover: ${leftover}');
+
+  // Part-5 (ii)
+  Object formInput = 'twenty-two';
+  if(formInput is String){
+    print('This is text');
+  }
+  if(formInput is ! int){
+    print('Input is confirmed Not to be integer!');
+  }
+
+  // Part-5(iii)
+  var reportBuffer = StringBuffer()
+  ..write('Report: ${courseTitle}')
+  ..write(' | Cap: ${capacity}')
+  ..write(' | Roster: ${enrolledStudents.length}');
+  print(reportBuffer.toString());
+
+  // part-5(iv)
+  List<String>? extraNotes;
+  extraNotes?..add('Room change pending');
+  print('Extra notes: $extraNotes');
+
+  // part-5 (v)
+  int? bonusSeats;
+  bonusSeats ??= 0;
+  print(bonusSeats);
 }
 
 
