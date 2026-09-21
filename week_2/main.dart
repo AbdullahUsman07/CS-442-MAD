288855bb890df51c81f1bdc32a2a5d553c7a1763
Abdullah Usman <179724875+AbdullahUsman07@users.noreply.github.com>
2026-09-21 11:10:56 +0500

    feat: Week_2 (Dart Fundamentals) upto Part 4 Completed!

---
+++ week_2/main.dart
@@ -0,0 +1,60 @@
+void main(List<String> args) {
+  printWelcome("Course Roster Manager");
+
+  // part-2
+  const int maxCapacity = 4;
+  final DateTime createdAt = DateTime.now();
+  String courseTitle = 'CS442: Mobile Application Developement';
+  int capacity = maxCapacity;
+  double creditHours = 3.0;
+  bool isOpen = true;
+  List<String> enrolledStudents = ['Saqlain', 'Ahmed', 'Hanan'];
+  Set<String> waitList = {'Jamil', 'Usman'};
+  Map<String, int> attendanceCount = {'Saqlain': 3, 'Ahmed': 4, 'Hanan': 5};
+
+  // Printing (on-console) using Interpolated String
+  print(
+    "${courseTitle} | Capacity: ${capacity} | Enrolled: ${enrolledStudents.length}",
+  );
+
+  // part-3
+
+  // part-3 (i, ii, iv)
+  String? instructorEmail;
+  print('Instructor Email: ${instructorEmail ?? 'TBA'}');
+
+  // ---------- trying to crash the code
+  //print(instructorEmail!);
+
+  late String enrollmentCode;
+
+  enrollmentCode = generateCode(courseTitle);
+  print("Enrollment Code: ${enrollmentCode}");
+
+  // Part-4 (Formatting Strings)
+  String rawNames = ' Saqlain, Hanan ,Ahmed , Jamil ';
+  List<String> cleanNames = [];
+  for (var name in rawNames.split(',')) {
+    cleanNames.add(name.trim());
+  }
+
+  print('CleanNames: $cleanNames');
+
+  String courseDescription =
+      '''
+Course: $courseTitle
+Credits: $creditHours
+''';
+  print(courseDescription);
+  print('Seats left: ${capacity - enrolledStudents.length}');
+}
+
+
+/// printWelcome function simply take a string as a parameter and displays that string in the console
+void printWelcome(String appName) {
+  print("===$appName===");
+}
+
+// Small setup function to generate code (used in part-3)
+String generateCode(String title) =>
+    title.substring(0, 2).toUpperCase() + '101';
